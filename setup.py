56d13dfaeafd5c6ab51df6bcaa0c5ee7b61d4b5e
Paul Morenkov <paulmorenkov@gmail.com>
2022-09-06 11:09:49 -0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mito_seg_py',
-    version='0.0.2',
+    version='0.0.3',
     author='Paul Morenkov',
     packages=['mito_seg_py'],
     install_requires=[
